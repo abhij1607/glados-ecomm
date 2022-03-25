29171f32a7d4902e7bfc177292b655b0c8bdf657
Abhi Jain <abhijain9807@gmail.com>
2022-03-26 01:01:59 +0530

    FIX-Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import logo from "./logo.png";
-import { Nav } from "./components";
+import { Nav, Footer } from "./components";
 import { Home } from "./pages";
 import "./styles/index.css";
 
