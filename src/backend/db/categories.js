7316f027cbe3e524a9f24970dee12c4849b17ebe
Abhi Jain <abhijain9807@gmail.com>
2022-03-26 15:57:51 +0530

    feat categories-context

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,19 +8,31 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
+    categoryName: "PlayStation",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "Xbox",
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "Nintendo",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Board Games",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "PC Gaming",
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
   },
