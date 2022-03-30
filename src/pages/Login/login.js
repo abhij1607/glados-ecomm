50adfb412238a4cbc89a8857170d1c42b6f9d359
Abhi Jain <abhijain9807@gmail.com>
2022-03-30 17:14:11 +0530

    WIP-basic login

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,11 +1,33 @@
 import "./login.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [response, setResponse] = useState("");
+
+  const loginRequest = async (e) => {
+    e.preventDefault();
+    const data = { email, password };
+    console.log(data);
+    try {
+      const loginResponse = await axios.post("/api/auth/login", data);
+      console.log(loginResponse);
+      setResponse(loginResponse.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {});
+
   return (
     <div className="wrapper wrapper-signup">
       <main className="align-center">
         <div className="box-shadow form-box auto-container align-center">
           <h1 className="h2">Login</h1>
-          <form action="#">
+          <form onSubmit={loginRequest}>
             <div className="input-box">
               <label className="label p-md txt-wt-md" htmlFor="email">
                 Enter your email:
@@ -16,6 +38,8 @@ const Login = () => {
                 type="email"
                 id="email"
                 name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="input-box">
@@ -28,6 +52,8 @@ const Login = () => {
                 type="password"
                 id="pwd"
                 name="pwd"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="input-checkbox">
