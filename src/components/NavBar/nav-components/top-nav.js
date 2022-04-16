e3456ffd3ef9ad228997f8b4b84bdcdea86ecaa9
Abhi Jain <abhijain9807@gmail.com>
2022-04-16 23:17:30 +0530

    [refactor] -updated folder structure for nav

--- src/components/NavBar/nav-components/top-nav.js
+++ src/components/NavBar/nav-components/top-nav.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useUserProducts } from "../../context/user-products-context";
+import { useUserProducts } from "../../../context/user-products-context";
 import { useState } from "react";
 
 const TopNav = () => {
