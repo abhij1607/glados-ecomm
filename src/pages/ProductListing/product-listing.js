16a4332e4a37d6599163cfd36ed6c2716b6bacd7
Abhi Jain <abhijain9807@gmail.com>
2022-06-16 09:29:28 +0530

    [refactor]- added nav-categories in product listing

--- src/pages/ProductListing/product-listing.js
+++ src/pages/ProductListing/product-listing.js
@@ -1,13 +1,17 @@
 import "./product-listing.css";
 import { Filter } from "./side-filter";
 import { Products } from "./products";
+import { NavCategories } from "../../components/NavBar/nav-components/nav-categories";
 
 const ProductListing = () => {
   return (
-    <div className="wrapper">
-      <Filter />
-      <Products />
-    </div>
+    <>
+      <NavCategories />
+      <div className="wrapper">
+        <Filter />
+        <Products />
+      </div>
+    </>
   );
 };
 export { ProductListing };
