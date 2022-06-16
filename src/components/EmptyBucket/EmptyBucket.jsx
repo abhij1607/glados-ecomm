f9e192cada6e095650bf7cf5f0f74a4022d69d11
Abhi Jain <abhijain9807@gmail.com>
2022-06-16 10:14:59 +0530

    [feat] - EmptyBucket

---
+++ src/components/EmptyBucket/EmptyBucket.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const EmptyBucket = () => {
+  return (
+    <div className="flex-column ">
+      <img
+        src="https://rukminim1.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png"
+        alt="Empty cart"
+        width="222"
+        height="162"
+        className="align-center"
+      />
+      <p className="text-center">Your Bucket is Empty</p>
+      <div className="text-center">
+        <Link className="btn btn-primary" to="/">
+          Shop Now
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export { EmptyBucket };
--- src/pages/Cart/cart.js
+++ src/pages/Cart/cart.js
@@ -1,6 +1,7 @@
 import { CartProductCards } from "./cart-components/cart-product-card";
 import { BillCard } from "./cart-components/bill-card";
 import { useAuth } from "../../context/auth-context";
+import { EmptyBucket } from "../../components/EmptyBucket/EmptyBucket";
 
 import "./cart.css";
 
@@ -8,11 +9,14 @@ const Cart = () => {
   const { userState } = useAuth();
   return (
     <>
-      <h1 className="text-center">Cart</h1>
-      <div className="wrapper wrapper-cart">
-        <CartProductCards />
-        {userState?.userDetails?.cart?.length > 0 && <BillCard />}
-      </div>
+      <main className="main-pane auto-container">
+        <h1 className="text-center">Cart</h1>
+        {userState?.userDetails?.cart?.length === 0 && <EmptyBucket />}
+        <div className="wrapper wrapper-cart">
+          <CartProductCards />
+          {userState?.userDetails?.cart?.length > 0 && <BillCard />}
+        </div>
+      </main>
     </>
   );
 };
