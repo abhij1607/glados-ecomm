7c14416ff7739762d9368d754007a999343bc23c
Abhi Jain <abhijain9807@gmail.com>
2022-06-18 12:09:45 +0530

    [feat] - SearchBox

--- src/App.js
+++ src/App.js
@@ -9,18 +9,39 @@ import {
   PageNotFound,
   Checkout,
   OrderSummary,
+  SearchResult,
 } from "./pages";
 import "./styles/index.css";
 import { Routes, Route } from "react-router-dom";
 import { RequiresAuth } from "./RequireAuth/RequiresAuth";
+import { useData } from "./context/data-context";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const { dataDispatch } = useData();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const productsResponse = await axios.get("/api/products");
+        dataDispatch({
+          type: "PRODUCTS",
+          payload: productsResponse.data.products,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductListing />} />
+        <Route path="/results" element={<SearchResult />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
 
